9c8fa64c548edca86623602f2bfaa3872b88ccd4
K.Chanatipz <chanatipzaza2003@gmail.com>
2024-07-09 22:17:58 +0700

    fix seed

--- seed/index.js
+++ seed/index.js
@@ -2,13 +2,7 @@
 const Campground = require("../models/campgroundModel");
 const Review = require("../models/reviewModel");
 const cities = require("./cities");
-const {
-  places,
-  descriptors,
-  images,
-  images2,
-  owners,
-} = require("./sampleData");
+const { places, descriptors, images, owners } = require("./sampleData");
 
 // const sampleImg = async () => {
 //   try {
@@ -25,22 +19,15 @@ const {
 //   }
 // };
 
-const frontURL = "https://images.unsplash.com/photo-";
-const backURL =
-  "?crop=entropy&cs=srgb&fm=jpg&ixid=M3w2MjU4OTl8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTkzMjYxNzN8&ixlib=rb-4.0.3&q=85";
 const baseURL = "https://res.cloudinary.com/dbi2rnjhe/image/upload/";
 
 const sampleData = (array) => array[Math.floor(Math.random() * array.length)];
 
 const sampleName = () => `${sampleData(descriptors)} ${sampleData(places)}`;
 
-const sampleLocation = () => {
-  const randIdx = Math.floor(Math.random() * 1000);
-  return `${cities[randIdx].city}, ${cities[randIdx].state}`;
-};
+const sampleCities = () => sampleData(cities);
 
-const sampleImage = () => `${frontURL}${sampleData(images)}${backURL}`;
-const sampleImage2 = () => `${baseURL}${sampleData(images2)}`;
+const sampleImage = () => `${baseURL}${sampleData(images)}`;
 
 const sampleDescription = () => {
   const text =
@@ -58,20 +45,24 @@ const makeDB = async () => {
   await Review.deleteMany({});
 
   for (let i = 0; i < 50; i++) {
+    const city = sampleCities();
+
     const camp = new Campground({
       name: sampleName(),
-      location: sampleLocation(),
-      // image: sampleImage(),
+      location: `${city.city}, ${city.state}`,
       images: [
         {
-          url: sampleImage2(),
+          url: sampleImage(),
           filename: "",
         },
       ],
       description: sampleDescription(),
-      price: samplePrice(20),
+      price: samplePrice(50),
       owner: sampleOwner(),
-      geometry: { type: "Point", coordinates: [-85.66039, 30.15522] },
+      geometry: {
+        type: "Point",
+        coordinates: [city.longitude, city.latitude],
+      },
     });
 
     await camp.save();
